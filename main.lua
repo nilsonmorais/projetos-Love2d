dcea217c76727bdb4f0626449eff550ba3ed8c1e
Nilson Morais <darkstrikerd@gmail.com>
2012-02-16 13:41:22 -0300

    Adicionado aceleracao

--- main.lua
+++ main.lua
@@ -1,44 +1,46 @@
-
 function love.load()
 	love.graphics.setColor(0,0,0)
 	love.graphics.setBackgroundColor( 255, 255, 255 )
 	car = love.graphics.newImage("car.png")
-	x = 150
+	x = 150 		--Posicao inicial
 	y = 150
-	angle = 90
-	speed = 1
-	maxspeed = 100
-	angledif = 10
-	salto=10
-	width = car:getWidth()
+	angle = 90 		--Angulo inicial
+	speed = 1 		--Velocidade
+	maxspeed = 100 	--Velocidade Max
+	angledif = 10	--Variacao de Angulo
+	acceleration =0				--aceleracao
+	max_acceleration=5
+	width = car:getWidth()		--Dimensoes do carro
    	height = car:getHeight()
 end
 
 function love.update(dt)
-   if love.keyboard.isDown("right") then
+	if love.keyboard.isDown("right") then
 		angle = angle + angledif
-   elseif love.keyboard.isDown("left") then
+   	elseif love.keyboard.isDown("left") then
 		angle = angle - angledif
-   end
-	if angle > 360 then
-		angle = 0
-	end
-	if angle < -360 then
-		angle = 0
-	end
+   	end
+
+	if angle > 360 or angle < -360 then angle = 0 end
+
 	scale_x = math.cos(math.rad(angle+90))
 	scale_y	= math.sin(math.rad(angle+90))
-	velocity_x = speed * scale_x
-	velocity_y = speed * scale_y
 
-   if love.keyboard.isDown("down") then
-      y = y + velocity_y
-      x = x + velocity_x
-   elseif love.keyboard.isDown("up") then
-      y = y - velocity_y
-      x = x - velocity_x
-   end
+	velocity_x = speed * scale_x * acceleration --Define velocidade no eixo X
+	velocity_y = speed * scale_y * acceleration
 
+   	if love.keyboard.isDown("down") then
+    	y = y + velocity_y
+    	x = x + velocity_x
+		acceleration = acceleration-0.3
+   	elseif love.keyboard.isDown("up") then
+    	y = y - velocity_y
+      	x = x - velocity_x
+		acceleration = acceleration+0.4
+   	end
+	acceleration=acceleration-0.3
+	if acceleration < 0 then acceleration = 0 end
+	if acceleration > max_acceleration then acceleration = max_acceleration end
 end
 
 function love.draw()
@@ -47,6 +49,5 @@ function love.draw()
 	love.graphics.print("Speed: " .. speed,10,40)
 	love.graphics.print("X_Velocity: " .. velocity_x,10,60)
 	love.graphics.print("Y_Velocity: " .. velocity_y,10,80)
-	love.graphics.print("sen: " .. math.sin(angle),10,100)
-	love.graphics.print("cos: " .. math.cos(angle),10,120)
+	love.graphics.print("Acceleration: " .. acceleration,10,100)
 end
